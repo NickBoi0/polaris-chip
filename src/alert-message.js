568bd2fd8b9d3fa5d1b19825a895230d56d959b7
NickFalletta <nickyny29@optonline.net>
2024-03-10 15:09:54 -0400

    changes

--- src/alert-message.js
+++ src/alert-message.js
@@ -9,62 +9,123 @@ export class AlertMessage extends LitElement {
   constructor() {
     super();
     this.sticky = false;
+    this.opened = true;
+    this.status = "notice";
+    this.date = '';
+    this.message = 'ALERT MESSAGE';
+
+    const storedStatus = localStorage.getItem('alertStatus');
+    if (storedStatus === 'closed') {
+      this.opened = false;
+      this.style.setProperty('--custom-alert-height', '25px');
+    }  
   }
+
 
   static get styles() {
     return css`
-
-      :host {
-        margin: 0;
+      :host([sticky]) {
+        position: sticky;
+        top: 0;
       }
-      :host([sticky]) .banner_wrapper {
-        position: fixed;
+
+      :host([open]) .banner_wrapper {
+        max-height: var(--custom-alert-height);
+      }
+
+      :host([status="notice"]) .banner_wrapper {
+        background-color: var(--custom-alert-notice-bg, var(--custom-alert-bg, #a10000));
+      }
+
+      :host([status="warning"]) .banner_wrapper {
+        background-color: var(--custom-alert-warning-bg, var(--custom-alert-bg, #007a2d));
+      }
+
+      :host([status="alert"]) .banner_wrapper {
+        background-color: var(--custom-alert-alert-bg, var(--custom-alert-bg, #002d70));
+      }
+
+      .closed .banner_wrapper {
+        height: var(--custom-alert-closed-height, var(--custom-alert-height, 25px));
       }
 
       .banner_wrapper {
         background: #a10000;
         padding: 20px;
-        top: 0;
-        width: 100%;
+        height: var(--custom-alert-height, 100px);
         border: 2px solid black;
+        transition: all 0.2s ease;
       }
 
       .banner {
-        max-width: 500px;
-        margin: 0 auto;
         display: flex;
-        align-items: center;
       }
 
-      .banner__text {
-        flex-grow: 1;
-        line-height: 1.4;
-        font-family: "Quicksand", sans-serif;
+      .banner-heading {
         text-align: center;
+        color: white;
+        flex: 1;
       }
       
+      .banner-opened-text {
+        text-align: center;
+        color: white;
+      }
       
+      .toggle-button {
+        padding: 5px;
+        border: 2px solid black;
+        background: red;
+        color: #fff;  
+        font-size: 14px;
+      }
 
     `;
   }
 
+  sizeBanner() {
+    this.opened = !this.opened;
+  
+    if (!this.opened) {
+      this.style.setProperty('--custom-alert-height', '25px');
+      localStorage.setItem('alertStatus', 'closed');
+    } else {
+      this.style.removeProperty('--custom-alert-height');
+      localStorage.removeItem('alertStatus');
+    }
+
+    this.requestUpdate();
+  }  
+
   render() {
     return html`   
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
       <div class="banner_wrapper">
         <div class="banner">
-          <div class="banner__text">
+          <div class="banner-heading">
             <strong>AlertMessage:</strong> Weather in State College is wack
           </div>
-          <span class="material-symbols-outlined">expand_more</span>
+          <button class="toggle-button" @click="${this.sizeBanner}">
+            ${this.opened ? 'Close' : 'Open'} Alert
+          </button>
+        </div>
+        <div class="banner-opened-text">
+          ${this.opened ? html`
+            <div class="message">${this.message}</div>
+            <div class="date">${this.date}</div>
+          ` : ''}
         </div>
       </div>
     `;
   }
+  
 
   static get properties() {
     return {
       sticky: { type: Boolean, reflect: true},
+      open: { type: Boolean, reflect: true },
+      status: { type: String },
+      date: { type: String },
+      message: { type: String },
     };
   }
 }
